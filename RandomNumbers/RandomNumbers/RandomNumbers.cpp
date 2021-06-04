e3d93c27215ff0faa1f0fc6982e08cb1f5b20788
Joel Ring <joel.ring@gmail.com>
2021-06-04 08:43:20 +0200

    creates random number project

---
+++ RandomNumbers/RandomNumbers/RandomNumbers.cpp
@@ -0,0 +1,26 @@
+// RandomNumbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+/*
+
+
+Create a large number of random numbers between 1 and 20!
+Count how often every number appears
+Compare this to the ideal distribution
+(i.e. display probability of every number or something like that)
+maybe, show the average, absolute discrepancy off the expected value
+Number 2: 189 times (4.975%)
+Number 3: 191 times (5.025%)
+...
+Maximum Offet: Number 4 (0.004%)
+Minimum Offet: Number 7 (0.0%)
+Average Offet: 0.002%
+
+*/
