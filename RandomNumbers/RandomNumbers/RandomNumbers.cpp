2d0d282746cca68f676bcc0e721e9c5fa2973b2c
Joel Ring <joel.ring@gmail.com>
2021-06-04 10:41:48 +0200

    adds statistics

--- RandomNumbers/RandomNumbers/RandomNumbers.cpp
+++ RandomNumbers/RandomNumbers/RandomNumbers.cpp
@@ -10,23 +10,38 @@ int main()
 {
     cout << GREEN;
     int randomNumberDistribution[20]{};
+    float deltaFromIdealValues[20]{};
     srand(time(NULL));
     cout << "how many random numbers?";
-    int iterations = 0, countNumbers=0;
+    int iterations = 0;
     cin >> iterations;
 
     for (int i = 0; i < iterations; i++) {
-        countNumbers++;
 
         auto randomInt = rand() % 20 + 1;
         randomNumberDistribution[randomInt - 1]++;
     }
     
-    cout <<endl<< "generated " << countNumbers << " random numbers:"<<endl;
+    cout <<endl<< "generated " << iterations << " random numbers"<<endl;
 
+    const float expectedPercentage = 5;
+    cout << "expected percentage:" << expectedPercentage <<"%"<<endl;
+    float maxDelta = 0, minDelta = 100, sumDelta = 0;;
+    cout << "deltas (absolute values): " << endl;
+    for (int i = 0;i < sizeof(randomNumberDistribution) / sizeof(int);i++) {
+        deltaFromIdealValues[i] = abs((100 * randomNumberDistribution[i] / (float)iterations) - expectedPercentage);
+        cout << deltaFromIdealValues[i] << " ";
+        maxDelta = deltaFromIdealValues[i] > maxDelta ? deltaFromIdealValues[i] : maxDelta;
+        minDelta = deltaFromIdealValues[i] < minDelta ? deltaFromIdealValues[i] : minDelta;
+        sumDelta += deltaFromIdealValues[i];
+    }
+    cout <<endl<< "max delta: " << maxDelta << endl;
+    cout << "min delta: " << minDelta << endl;
+    cout << "sum of all deltas: " << sumDelta << endl;
+    cout << "average delta: " << sumDelta/20.0f << endl;
 
     for (int i = 0;i < sizeof(randomNumberDistribution)/sizeof(int);i++) {
-        cout << "Number " << i+1<<": "<< randomNumberDistribution[i]<< " times ("<< 100*randomNumberDistribution[i]/ (float)countNumbers<<"%)"<< endl;
+        cout << "Number " << i+1<<": "<< randomNumberDistribution[i]<< " times ("<< 100*randomNumberDistribution[i]/ (float)iterations <<"%)"<< endl;
     }
 }
 
